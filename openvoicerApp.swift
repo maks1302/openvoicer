f32aedc45adcebbc599a7940973920e1f61bccf9
max <max13021302@gmail.com>
2026-08-25 11:56:16 +0300

    first commit

---
+++ openvoicerApp.swift
@@ -0,0 +1,17 @@
+//
+//  openvoicerApp.swift
+//  openvoicer
+//
+//  Created by Maksym Dziura on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct openvoicerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
